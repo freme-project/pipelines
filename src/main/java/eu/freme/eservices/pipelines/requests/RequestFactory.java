434f25941521be789db1e447cc9e0c111bc9b5fa
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2015-07-31 15:19:56 +0200

    Added some documentation; closes #2.

--- src/main/java/eu/freme/eservices/pipelines/requests/RequestFactory.java
+++ src/main/java/eu/freme/eservices/pipelines/requests/RequestFactory.java
@@ -17,7 +17,7 @@ public class RequestFactory {
 	 * Creates a default request to the e-Entity Spotlight service.
 	 * @param text		The text to enrich (plain text).
 	 * @param language	The language the text is in.
-	 * @return			A request that can still be modified.
+	 * @return			A request for e-Entity Spotlight that can still be modified.
 	 */
 	public static SerializedRequest createEntitySpotlight(final String text, final String language) {
 		RequestBuilder builder = new RequestBuilder(ServiceConstants.E_ENTITY_SPOTLIGHT.getUri());
@@ -32,7 +32,7 @@ public class RequestFactory {
 	 * @param text		The text to enrich (plain text).
 	 * @param language  The language the text is in.
 	 * @param dataSet   The data set to use for enrichment.
-	 * @return        	A request that can still be modified.
+	 * @return        	A request for e-Entity FREME NER that can still be modified.
 	 */
 	public static SerializedRequest createEntityFremeNER(final String text, final String language, final String dataSet) {
 		RequestBuilder builder = new RequestBuilder(ServiceConstants.E_ENTITY_FREME_NER.getUri());
@@ -44,6 +44,11 @@ public class RequestFactory {
 				.build();
 	}
 
+	/**
+	 * Creates a default request to the e-Link service.
+	 * @param templateID	The template ID to use for linking.
+	 * @return				A request for e-Link that can still be modified
+	 */
 	public static SerializedRequest createLink(final String templateID) {
 		RequestBuilder builder = new RequestBuilder(ServiceConstants.E_LINK.getUri());
 		return builder
@@ -51,10 +56,20 @@ public class RequestFactory {
 				.build();
 	}
 
+	/**
+	 * Converts a list of requests to JSON.
+	 * @param requests	The list of requests to convert to JSON.
+	 * @return			A JSON string representing serialized requests, which can be sent to the Pipelines API.
+	 */
 	public static String toJson(final List<SerializedRequest> requests) {
 		return gson.toJson(requests);
 	}
 
+	/**
+	 * Converts a JSON string to a list of requests.
+	 * @param serializedRequests	The JSON string of requests to convert.
+	 * @return						The list of requests represented by the JSON string.
+	 */
 	public static List<SerializedRequest> fromJson(final String serializedRequests) {
 		SerializedRequest[] requests = gson.fromJson(serializedRequests, SerializedRequest[].class);
 		return Arrays.asList(requests);
