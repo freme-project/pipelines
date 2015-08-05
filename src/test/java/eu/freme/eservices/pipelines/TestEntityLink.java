f0a7a692e0dae9c3df7d08f8d13b4f57b172dc2c
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2015-08-05 09:47:16 +0200

    Added test for FREME-NER in the e-Entity step.

--- src/test/java/eu/freme/eservices/pipelines/TestEntityLink.java
+++ src/test/java/eu/freme/eservices/pipelines/TestEntityLink.java
@@ -12,8 +12,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-//import eu.freme.conversion.rdf.RDFConstants;
-
+import static org.junit.Assert.assertEquals;
 /**
  * @author Gerald Haesendonck
  */
@@ -24,7 +23,7 @@ public class TestEntityLink extends LocalServerTestBase {
 		//String data = "A court in Libya has sentenced Saif al-Islam Gaddafi, son of deposed leader Col Muammar Gaddafi, and eight others to death over war crimes linked to the 2011 revolution.";
 		String data = "This summer there is the Zomerbar in Antwerp, one of the most beautiful cities in Belgium.";
 		SerializedRequest entityRequest = RequestFactory.createEntitySpotlight(data, "en");
-		SerializedRequest linkRequest = RequestFactory.createLink("3");	// just the first template, to test.
+		SerializedRequest linkRequest = RequestFactory.createLink("3");	// Geo pos
 
 		List<SerializedRequest> serializedRequests = Arrays.asList(entityRequest, linkRequest);
 
@@ -32,24 +31,27 @@ public class TestEntityLink extends LocalServerTestBase {
 				.body(new JsonNode(RequestFactory.toJson(serializedRequests)))
 				.asString();
 		System.out.println("response.getStatus() = " + response.getStatus());
+		assertEquals("Wrong status code", 200, response.getStatus());
 		System.out.println("response.getStatusText() = " + response.getStatusText());
 		System.out.println("response.getBody() = " + response.getBody());
 		Unirest.shutdown();
 	}
 
-/*	@Test
+	@Test
 	public void testFremeNER() throws UnirestException, IOException {
-		String data = "A court in Libya has sentenced Saif al-Islam Gaddafi, son of deposed leader Col Muammar Gaddafi, and eight others to death over war crimes linked to the 2011 revolution.";
+		String data = "This summer there is the Zomerbar in Antwerp, one of the most beautiful cities in Belgium.";
+		SerializedRequest linkRequest = RequestFactory.createLink("3");	// Geo pos
 
-		SerializedRequest entityRequest = RequestFactory.createEntityFremeNER(data, "en");
-		List<SerializedRequest> serializedRequests = Arrays.asList(entityRequest);
+		SerializedRequest entityRequest = RequestFactory.createEntityFremeNER(data, "en", "viaf");
+		List<SerializedRequest> serializedRequests = Arrays.asList(entityRequest, linkRequest);
 
 		HttpResponse<String> response = Unirest.post("http://localhost:9000/pipelining/chain")
 				.body(new JsonNode(RequestFactory.toJson(serializedRequests)))
 				.asString();
 		System.out.println("response.getStatus() = " + response.getStatus());
+		assertEquals("Wrong status code", 200, response.getStatus());
 		System.out.println("response.getStatusText() = " + response.getStatusText());
 		System.out.println("response.getBody() = " + response.getBody());
 		Unirest.shutdown();
-	}*/
+	}
 }
