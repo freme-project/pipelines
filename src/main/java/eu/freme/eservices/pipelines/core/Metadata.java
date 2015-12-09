20db91a0391ff8527b7402d067d236abbcc378ce
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2015-12-09 10:48:24 +0100

    Renamed serviceToDuration to serviceDurations for clarity.

--- src/main/java/eu/freme/eservices/pipelines/core/Metadata.java
+++ src/main/java/eu/freme/eservices/pipelines/core/Metadata.java
@@ -10,11 +10,11 @@ import java.util.Map;
  * @author Gerald Haesendonck
  */
 public class Metadata {
-	private final Map<String, Long> serviceToDuration;
+	private final Map<String, Long> serviceDurations;
 	private final long totalDuration;
 
 	public Metadata(Map<String, Long> serviceToDuration, long totalDuration) {
-		this.serviceToDuration = serviceToDuration;
+		this.serviceDurations = serviceToDuration;
 		this.totalDuration = totalDuration;
 	}
 }
